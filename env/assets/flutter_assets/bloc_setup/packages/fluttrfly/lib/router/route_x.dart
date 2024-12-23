dc80e23e10d75e0e861b12b62879beb03a241989
bharatheasycloud <bharath@easycloud.in>
2024-12-23 16:11:47 +0530

    route_x updated and logo of app changed.

--- env/assets/flutter_assets/bloc_setup/packages/fluttrfly/lib/router/route_x.dart
+++ env/assets/flutter_assets/bloc_setup/packages/fluttrfly/lib/router/route_x.dart
@@ -4,21 +4,39 @@ import 'package:go_router/go_router.dart';
 class RouteX {
   final String pageName;
   final RouteX? parent;
-  late String path;
-  late String name;
+  late final String path;
+  late final String name;
+  late String? paramsKey;
 
   RouteX({
     required this.pageName,
     this.parent,
     String? path,
     String? name,
+    this.paramsKey,
   }) {
-    this.name = name ?? pageName.split('/').last;
-    this.path = pageName == '/'
-        ? pageName
-        : parent != null
-            ? '${parent!.path}/$pageName'
-            : '/$pageName';
+    this.name = name ?? pageName;
+    this.path = path ?? _generatePath();
+  }
+
+  // Constructor for routes with matched parameters
+  RouteX.matched({
+    required String pageName,
+    required String paramsKey,
+    RouteX? parent,
+  }) : this(
+          pageName: '$pageName/:$paramsKey',
+          parent: parent,
+          name: pageName,
+          paramsKey: paramsKey,
+        );
+
+  // Generates the path based on the parent route and page name
+  String _generatePath() {
+    if (pageName == '/') {
+      return pageName;
+    }
+    return parent != null ? '${parent!.path}/$pageName' : '/$pageName';
   }
 
   // Method to get the parent route path
@@ -26,15 +44,41 @@ class RouteX {
 }
 
 extension AppRouteNavigation on RouteX {
-  void go(BuildContext context, {Object? extra}) {
-    context.go(path, extra: extra);
+  /// Navigates to the route using `go`, optionally with parameters or extra data.
+  void go(BuildContext context, {Object? extra, String? params}) {
+    final String targetPath = (paramsKey != null && params != null)
+        ? path.replaceAll(':$paramsKey', params)
+        : path;
+    context.go(targetPath, extra: extra);
   }
 
-  void goNamed(BuildContext context, {Object? extra}) {
-    context.goNamed(path.split('/').last, extra: extra);
+  /// Navigates to the route using `goNamed`, allowing for named routes and optional parameters.
+  void goNamed(BuildContext context, {Object? extra, String? params}) {
+    if (paramsKey != null && params != null) {
+      context.goNamed(name, pathParameters: {paramsKey!: params}, extra: extra);
+      return;
+    } else {
+      context.goNamed(name, extra: extra);
+    }
   }
 
-  Future<T?> push<T>(BuildContext context, {Object? extra}) async {
-    return await context.push(path, extra: extra);
+  /// Pushes a new route onto the stack using `push`, with optional parameters and extra data.
+  Future<T?> push<T>(BuildContext context,
+      {Object? extra, String? params}) async {
+    final String targetPath = (paramsKey != null && params != null)
+        ? path.replaceAll(':$paramsKey', params)
+        : path;
+    return await context.push(targetPath, extra: extra);
+  }
+
+  /// Pushes a new named route onto the stack using `pushNamed`, with optional parameters and extra data.
+  Future<T?> pushNamed<T>(BuildContext context,
+      {Object? extra, String? params}) async {
+    if (paramsKey != null && params != null) {
+      return await context.pushNamed(name,
+          pathParameters: {paramsKey!: params}, extra: extra);
+    } else {
+      return await context.pushNamed(name, extra: extra);
+    }
   }
 }
--- env/assets/flutter_assets/riverpod_setup/lib/main.dart
+++ env/assets/flutter_assets/riverpod_setup/lib/main.dart
@@ -8,9 +8,12 @@ import 'bootstrap.dart';
 /// Read and complete this steps before running app
 /// Step 1 : Convert every relative import to package import...
 /// Step 2 : Add real [Urls] at lib/core/consts/urls.dart in [Urls.uat] and [Urls.local] factory's
+/// Step 3 : Run: flutter pub get && flutter pub run flutter_launcher_icons and Delete [/android/app/src/main/res/minmap-anydpi-v26]
+///          To get icon for app ok.
 
 Future<void> main() async => bootstrap(() {
       final container = ProviderContainer();
       runApp(
           UncontrolledProviderScope(container: container, child: const App()));
     });
+ 
